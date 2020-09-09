1a8b6b400e89b44b29e13c3dc7b2ef56604c28df
Mahad Zaryab <mahadzaryab1@gmail.com>
2020-09-09 10:55:26 -0400

    Added new line characters for styling

--- portal_api/app.py
+++ portal_api/app.py
@@ -9,4 +9,4 @@ api = Api(app)
 api.add_resource(Health, '/health')
 
 if __name__ == "__main__":
-    app.run(port=8080, host= '0.0.0.0', debug=True)
+    app.run(port=8080, host='0.0.0.0', debug=True)
